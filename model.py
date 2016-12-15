8436ae639bc0a9f0e69c5605174ee7394e40799b
BigSako <evebigsako@gmail.com>
2016-12-15 20:36:49 +0100

    Fix

--- model.py
+++ model.py
@@ -291,9 +291,6 @@ class MyDBModel:
                 if group not in messages_by_group:
                     messages_by_group[group] = []
 
-                if row['from_character'] == "EVILDEAD12" or row['from_character'] == "MERCIE1":
-                    continue
-
                 messages_by_group[group].append(
                     {
                         'id': row['id'],
